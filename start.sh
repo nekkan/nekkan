86afed7e231e5d95f1f0e931dafbc53ba140a630
Pedro <nxkkdev@gmail.com>
2021-01-04 22:35:49 +0000

    24

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-export JAVA_TOOL_OPTIONS=-Xmx32G
-java -Xmx32G -Xms31G -jar spigot-1.16.4.jar
+export JAVA_TOOL_OPTIONS=-Xmx16G
+
+java -Xmx16G -Xms15G -jar spigot-1.16.4.jar
+
 # ./ngrok tcp -region us 25565
